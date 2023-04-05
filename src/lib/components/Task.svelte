12f03201903b9c11198e24883f134959746d4c2e
Jack Goodall <jack@safecube.com>
2023-04-05 10:46:59 -0400

    add edit icon

--- src/lib/components/Task.svelte
+++ src/lib/components/Task.svelte
@@ -2,15 +2,16 @@
 	import Card from '$lib/components/Card.svelte';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import type { Task } from '@prisma/client';
-	import { Trash } from '@steeze-ui/heroicons';
+	import { Pencil, Trash } from '@steeze-ui/heroicons';
 
 	export let task: Task;
 </script>
 
 <Card>
-	<h3 slot="title" class="flex items-center justify-between">
+	<h3 slot="title" class="flex items-center">
 		{task.name}
-		<button on:click|preventDefault class="group-hover:block hidden">
+		<Icon src={Pencil} size="16" class="ml-2 stroke-gray-400 group-hover:block hidden" />
+		<button on:click|preventDefault class="group-hover:block hidden ml-auto">
 			<Icon src={Trash} size="16" class="stroke-rose-500" />
 		</button>
 	</h3>
